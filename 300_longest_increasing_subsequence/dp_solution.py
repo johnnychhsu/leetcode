d38a5351a23be14d66110eee01be3bd8add77a33
johnnychhsu <johnny.chhsu01@gmail.com>
2018-11-14 13:25:41 +0800

    add dp for 300

---
+++ 300_longest_increasing_subsequence/dp_solution.py
@@ -0,0 +1,11 @@
+def lengthOfLIS(nums):
+    dp = [0] * len(nums)
+    for i in range(len(nums)):
+        max_l = 0
+        for j in range(i):
+            if nums[j] > nums[i]:
+                max_l = max(max_l, dp[j])
+        dp[i] = max_l + 1
+    return max(dp)
+
+
